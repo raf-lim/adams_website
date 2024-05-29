8c4a078daad2aef8f48f6ff8f73280b1a5c4bf44
raf-lim <raf.sukiennik@gmail.com>
2024-05-29 20:06:06 +0200

    Add memory game template and set up view.

--- mysite/webgames/urls.py
+++ mysite/webgames/urls.py
@@ -3,5 +3,9 @@ from django.views.generic import TemplateView
 
 app_name = "webgames"
 urlpatterns = [
-    path("memory/", view=TemplateView.as_view(), name="memory")
+    path(
+        "memory/",
+        view=TemplateView.as_view(template_name="webgames/memory/memory.html"),
+        name="memory"
+    )
 ]
