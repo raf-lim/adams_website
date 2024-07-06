a05b3b524a46e2ebdfe5b4e67033855f9e2c35ec
raf-lim <raf.sukiennik@gmail.com>
2024-07-06 17:36:28 +0200

    Move collectfast before django.contrib.staticfiles.

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -128,7 +128,7 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 # Collectfast
 # ------------------------------------------------------------------------------
 # https://github.com/antonagestam/collectfast#installation
-INSTALLED_APPS = ["collectfast"] + INSTALLED_APPS  # noqa: F405
+# INSTALLED_APPS = ["collectfast"] + INSTALLED_APPS  # noqa: F405
 
 # LOGGING
 # ------------------------------------------------------------------------------
