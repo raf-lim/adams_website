d767103d9d307acfbcaa1a9e20fad8696b178198
raf-lim <raf.sukiennik@gmail.com>
2024-07-08 12:22:59 +0200

    Redirect user to home after successful login.

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -37,7 +37,8 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("home")
+        # return reverse("users:detail", kwargs={"username": self.request.user.username})
 
 
 user_redirect_view = UserRedirectView.as_view()
